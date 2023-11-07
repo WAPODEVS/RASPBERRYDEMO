9527ee904b6503eb445ea98f53c842e8907b3346
Cesar Giraldo <privado466@gmail.com>
2023-11-06 23:56:24 -0500

    Demo1 probado en Raspberry

--- demo1.py
+++ demo1.py
@@ -49,59 +49,65 @@ class Modbus:
             self.client.write_coil(address = coil,value = valor, slave = esclavo)
         except Exception as exc:
             print(f"Error de comunicacion Modbus: {exc}")
-                
-#programa principal   
+
+# Tabla menu
+def print_menu_table():
+    tabla_menu = PrettyTable()
+    tabla_menu.field_names = ["Opcion", "Descripcion"]
+    tabla_menu.add_row(["1", "Escribir registros"])
+    tabla_menu.add_row(["2", "Leer registros"])
+    tabla_menu.add_row(["3", "Salir"])
+    print(tabla_menu)
+
+# Prueba de escritura de registros
+def escribir_registros_demo():
+    registro = int(input("Ingrese la dirección del registro al que quiere escribir un valor : "))
+    valor = int(input("Ingrese el valor que quiere enviar al esclavo : "))
+    slaveID = int(input("Ingresar el ID del esclavo al que desea enviar el dato : "))
+    modbus0.escribir_registro(registro = registro, valor = valor, esclavo = slaveID)
+
+# Prueba de lectura de registros
+def leer_registros_demo():
+    registro_inicio = int(input("Ingrese la dirección del registro donde se iniciará la lectura : "))
+    num_registros = int(input("Ingrese la cantidad de registros que desea leer : "))
+    slaveID = int(input("Ingresar el ID del esclavo al cual desea leer los registros : "))
+
+    registros_esclavo = modbus0.leer_registros(registro_inicio = registro_inicio, num_registros = num_registros, esclavo = slaveID)
+
+    print("\nSe leyeron los siguientes registros : \n")
+    table = PrettyTable()
+    table.field_names = ["Registro", "Valor"]
+    for i, v in enumerate(registros_esclavo):
+        table.add_row([i + registro_inicio, v])
+    print(table)   
+
+# Comprando la conexión
+def verificar_conexion():
+    if modbus0.client.connect():
+        print("\n----CONEXION EXITOSA----\n")
+
+# Programa principal   
 if __name__ == "__main__":
 
-    escritura_registros_flag = True
-    lectura_registros_flag = True
-
     # Creacion del objeto modbus
-    modbus0 = Modbus(port = "COM4", timeout = 10, baudrate = 9600, bytesize = 8, parity = "N", stopbits = 1)
+    modbus0 = Modbus(port = "/dev/ttyUSB0", timeout = 10, baudrate = 9600, bytesize = 8, parity = "N", stopbits = 1)
 
-    if modbus0.client.connect():
-        print("================")
-        print("Conexión exitosa")
-        print("================\n")
+    print("======================")
+    print("        DEMO 1       ")
+    print("======================")
 
-    # Prueba de escritura de registros
-    print("================================")
-    print("Prueba de escritura de registros")
-    print("================================")
+    verificar_conexion()
 
-    while escritura_registros_flag:
+    while True:
+        print_menu_table()
+        opcion = input("Ingrese una opcion : ")
 
-        registro = int(input("Ingrese la dirección del registro al que quiere escribir un valor : "))
-        valor = int(input("Ingrese el valor que quiere enviar al esclavo : "))
-        slaveID = int(input("Ingresar el ID del esclavo al que desea enviar el dato : "))
-
-        modbus0.escribir_registro(registro = registro, valor = valor, esclavo = slaveID)
-
-        repeat = input("Desea seguir escribiendo registros? (Y/N) : ").upper()
-
-        escritura_registros_flag = True if repeat == 'Y' else False
-
-    # Prueba de lectura de registros
-
-    print("================================")
-    print("Prueba de lectura de registros")
-    print("================================")
-
-    while lectura_registros_flag:
-
-        registro_inicio = int(input("Ingrese la dirección del registro donde se iniciará la lectura : "))
-        num_registros = int(input("Ingrese la cantidad de registros que desea leer : "))
-        slaveID = int(input("Ingresar el ID del esclavo al cual desea leer los registros : "))
-
-        registros_esclavo = modbus0.leer_registros(registro_inicio = registro_inicio, num_registros = num_registros, esclavo = slaveID)
-
-        print("\nSe leyeron los siguientes registros : \n")
-        table = PrettyTable()
-        table.field_names = ["Registro", "Valor"]
-        for i, v in enumerate(registros_esclavo):
-            table.add_row([i, v])
-        print(table)
-        
-        repeat = input("Desea seguir leyendo registros? (Y/N) : ").upper()
-
-        lectura_registros_flag = True if repeat == 'Y' else False
+        if opcion not in ["1", "2", "3"]:
+            print("Esta opcion no existe. Vuelva a intentarlo.")
+        elif opcion == '1':
+            escribir_registros_demo()
+        elif opcion == '2':
+            leer_registros_demo()
+        else:
+            print("Programa terminado")
+            break
