67e3c32aa92df732db084d3769b0e30ef8554a75
Cesar Giraldo <privado466@gmail.com>
2023-11-17 11:16:50 -0500

    Demo 0 listo

--- demo0.py
+++ demo0.py
@@ -12,11 +12,14 @@ if __name__ == "__main__":
 
     # Creación del objeto modbus
     modbusConnection = ModbusSerialClient(port = "/dev/ttyUSB0", framer = ModbusRtuFramer, baudrate = 9600, bytesize = 8, 
-                                parity = "N", stopbits = 1)
+                        parity = "N", stopbits = 1)
     
     # Verificar la conexión
     if modbusConnection.connect():
         print("\n----CONEXION EXITOSA----\n")
 
     # Escritura de datos (0x06)
-    modbusConnection.write_register(address = 4, value = 10, slave = 5)
+    registro = int(input("Ingrese la dirección del registro al que quiere escribir un valor : "))
+    valor = int(input("Ingrese el valor que quiere enviar al esclavo : "))
+    slaveID = int(input("Ingresar el ID del esclavo al que desea enviar el dato : "))
+    modbusConnection.write_register(address = registro, value = valor, slave = slaveID)
